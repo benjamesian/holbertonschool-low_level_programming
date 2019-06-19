4b7ebe54e2a996abe1d6159b40ab3e0b9fdb2fa5
Ben Cook <808@holbertonschool.com>
2019-06-19 19:50:36 +0000

    Use Betty style documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point for program
+ *
+ * Description: program uses puts to print a string
+ * Return: 0
+ */
 int main(void)
 {
 char my_string[] = "\"Programming is like building a multilingual puzzle";
