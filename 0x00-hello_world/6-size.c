70af0c1d00b77f8241d347efb320597ac5973672
Ben Cook <808@holbertonschool.com>
2019-06-19 20:08:21 +0000

    Format code Betty style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Size of a char: %d bytes(s)\n", (int) sizeof(char));
-  printf("Size of an int: %d bytes(s)\n", (int) sizeof(int));
-  printf("Size of a long: %d bytes(s)\n", (int) sizeof(long));
-  printf("Size of a long int: %d bytes(s)\n", (int) sizeof(long int));
-  printf("Size of a long long int: %d bytes(s)\n", (int) sizeof(long long int));
-  printf("Size of a float: %d bytes(s)\n", (int) sizeof(float));
-
-  return (0);
+/**
+ * main - entry point for the function
+ *
+ * Description: Prints sizes of different types
+ * Return: 0
+ */
+int main(void)
+{
+printf("Size of a char: %d bytes(s)\n", (int) sizeof(char));
+printf("Size of an int: %d bytes(s)\n", (int) sizeof(int));
+printf("Size of a long: %d bytes(s)\n", (int) sizeof(long));
+printf("Size of a long int: %d bytes(s)\n", (int) sizeof(long int));
+printf("Size of a long long int: %d bytes(s)\n", (int) sizeof(long long int));
+printf("Size of a float: %d bytes(s)\n", (int) sizeof(float));
+return (0);
 }
