908f4a8dee576fd63b8ad0601caebc4cefd1db34
Ben Cook <808@holbertonschool.com>
2019-06-25 22:10:46 +0000

    change style to satisfy checker

---
+++ 0x02-functions_nested_loops/7-print_last_digit-old.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - Print and return the last digit of an integer
+ * @n: An integer
+ *
+ * Return: The last digit of the integer n
+ */
+int print_last_digit(int n)
+{
+	int _abs = n < 0 ? -n : n;
+	int last = _abs % 10;
+
+	_putchar('0' + last);
+	return (last);
+}
