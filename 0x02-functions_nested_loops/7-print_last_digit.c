d5d4a3e57e97d81dc572ca85abdf459eba139fd6
Ben Cook <808@holbertonschool.com>
2019-06-25 22:33:05 +0000

    handle overflow

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,11 @@
  */
 int print_last_digit(int n)
 {
-	int _abs, last;
+	int _abs = (n < 0) ? -n : n;
+	int last = _abs % 10;
 
-	if (n < 0)
-		_abs = -n;
-	else
-		_abs = n;
-
-	last = _abs % 10;
+	if (_abs < 0)
+		last = (last - 10 ) % 10;
 
 	_putchar('0' + last);
 	return (last);
