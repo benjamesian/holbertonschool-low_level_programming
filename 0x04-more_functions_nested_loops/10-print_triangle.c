acce5b1e51aded01eca0934cb8a11fa0d0193bd0
Ben Cook <808@holbertonschool.com>
2019-06-27 22:39:10 +0000

    attempt triangle printing task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (j < i)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
