a41392d0a164518046f61b8b4772838b88b40f4a
Ben Cook <808@holbertonschool.com>
2019-06-27 21:39:38 +0000

    attempted tasks 8 and 9

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_square - prints a square, followed by a new line
+ * @n: The size of the square
+ */
+void print_square(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - entry point
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 != 0)
+			printf("%d", i);
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
