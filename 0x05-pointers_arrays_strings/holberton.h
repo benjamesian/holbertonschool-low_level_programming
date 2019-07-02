b050369b6c983ff919988dc5c3a4c3df7f11a007
Ben Cook <808@holbertonschool.com>
2019-07-02 16:15:59 +0000

    added header

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,12 @@
+int _putchar(char);
+void reset_to_98(int*);
+void swap_int(int*, int*);
+void _puts(char*);
+void print_rev(char*);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int _strlen(char *s);
