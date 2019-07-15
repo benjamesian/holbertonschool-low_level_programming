1639e51b4e7cf4e374d9aef80078ab6981486961
Ben Cook <808@holbertonschool.com>
2019-07-15 18:13:15 +0000

    first pass advanced

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+
+int count_seps(char *, char);
+int count_until(char *, char);
+
+/**
+ * strtow - cut string into words array
+ * @str: string
+ *
+ * Return: pointer to array of words
+ */
+char **strtow(char *str)
+{
+	char **p, **tmp;
+	char *pc;
+	int i;
+	int nwords = 0;
+
+	if (!str || !*str)
+		return (NULL);
+
+	nwords = count_seps(str, ' ');
+	p = malloc((nwords + 1) * sizeof(*p));
+	if (!nwords || !p)
+		return (NULL);
+
+	tmp = p;
+
+	while (*str)
+	{
+		while (*str++ == ' ')
+			;
+		str--;
+		if (!*str)
+			break;
+		i = count_until(str, ' ');
+		*p = malloc(i * sizeof(**p));
+		if (!*p)
+			return (NULL);
+		pc = *p;
+		while (*str != ' ' && (*pc++ = *str++))
+			;
+		*pc = '\0';
+		p++;
+	}
+	*p = NULL;
+
+	return (tmp);
+}
+
+/**
+ * count_seps - count words
+ * @str: string
+ * @sep: word sep
+ *
+ * Return: number of words
+ */
+int count_seps(char* str, char sep)
+{
+	if (!*str)
+		return (0);
+
+	while (*str++ == sep)
+		;
+
+	if (*str)
+	{
+		while (*str && *str++ != sep)
+			;
+		return (1 + count_seps(str, sep));
+	}
+	else
+		return (0);
+}
+
+/**
+ * count_until - count until break
+ * @str: string
+ * @stop: when to stop counting
+ *
+ * Return: num chars until stop
+ */
+int count_until(char *str, char stop)
+{
+	int i = 0;
+	while (*str++ != stop)
+		i++;
+
+	return (i);
+}
