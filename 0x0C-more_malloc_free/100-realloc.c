0f8fb0ccfeb6c222010be3f4ac08804ce4029f94
Ben Cook <808@holbertonschool.com>
2019-07-18 22:12:09 +0000

    attempt task 100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocate memory
+ * @ptr: pointer to memory that is allocated
+ * @old_size: size of memory allocated at ptr
+ * @new_size: amount of memory to allocate
+ *
+ * Return: pointer to newly allocated memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *buffer;
+	unsigned char *stmp, *dtmp;
+	unsigned int mem_to_cpy;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	buffer = malloc(new_size);
+	if (buffer == NULL)
+		return (NULL);
+
+	mem_to_cpy = (new_size < old_size) ? new_size : old_size;
+
+	stmp = ptr;
+	dtmp = buffer;
+	while (mem_to_cpy--)
+		*dtmp++ = *stmp++;
+
+	free(ptr);
+
+	return (buffer);
+}
