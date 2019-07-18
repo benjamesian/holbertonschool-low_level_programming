cc946350c33a91fe834a4ad3af5d6dbf6e79d80b
Ben Cook <808@holbertonschool.com>
2019-07-18 20:40:16 +0000

    attempt task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - allocate memory for an array
+ * @nmemb: number of elements in array
+ * @size: size of an array element in bytes
+ *
+ * Return: pointer to start of array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *buffer;
+
+	if (nmemb < 1 || size < 1)
+		return (NULL);
+
+	buffer = malloc(size * nmemb);
+	if (buffer == NULL)
+		return (NULL);
+
+	return (buffer);
+}
