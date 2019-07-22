3193109a65ca0dde6e554e3d125a97a7f965a51c
Ben Cook <808@holbertonschool.com>
2019-07-22 22:32:10 +0000

    fail if strdup fails

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,8 +20,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	d->name = _strdup(name);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
 	d->age = age;
 	d->owner = _strdup(owner);
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
 
 	return (d);
 }
