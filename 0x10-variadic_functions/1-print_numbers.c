ac971ba3fd878c276bf6360ec03f0bbba61e473d
Ben Cook <808@holbertonschool.com>
2019-07-27 09:07:44 +0000

    handle print no numbers case

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,6 +11,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list valist;
 	unsigned int i;
 
+	if (n == 0)
+	{
+		putchar('\n');
+		return;
+	}
+
 	va_start(valist, n);
 
 	for (i = 1; i < n; i++)
