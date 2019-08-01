77fc89c9cff5b2efd93b12a8662ac2630453db13
Ben Cook <808@holbertonschool.com>
2019-08-01 23:45:27 +0000

    dont use strlen from string.h

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include "lists.h"
 
+unsigned int _strlen(const char *s);
+
 /**
  * add_node - add a new node at the beginning of a list_t list
  * @head: pointer to a pointer to the beginning of the list
@@ -19,9 +21,28 @@ list_t *add_node(list_t **head, const char *str)
 
 	temp->next = *head;
 	temp->str = strdup(str);
-	temp->len = strlen(str);
+	temp->len = _strlen(str);
 
 	*head = temp;
 
 	return (temp);
+}
+
+/**
+ * _strlen - get the length of a string
+ * @s: a string
+ *
+ * Return: The length of the string
+ */
+unsigned int _strlen(const char *s)
+{
+	unsigned int i;
+
+	if (s)
+		for (i = 0; s[i]; i++)
+			;
+	else
+		return (0);
+
+	return (i);
 }
