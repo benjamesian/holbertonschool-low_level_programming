2c47c66307199eb958a41220cfcfd8f8022573a8
Ben Cook <808@holbertonschool.com>
2019-08-06 07:46:36 +0000

    remove helper function

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,18 +6,9 @@
  */
 void free_listint2(listint_t **head)
 {
-	free_listint(*head);
-	*head = NULL;
-}
-
-/**
- * free_listint - free a linked list
- * @head: pointer to first element of the linked list
- */
-void free_listint(listint_t *head)
-{
-	if (!head)
+	if (!head || !*head)
 		return;
-	free_listint(head->next);
-	free(head);
+	free_listint2(&((*head)->next));
+	free(*head);
+	*head = NULL;
 }
