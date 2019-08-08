674b1c680071d6697a667c9574f5a5055b713ac6
Ben Cook <808@holbertonschool.com>
2019-08-08 14:16:53 -0700

    attempt task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+/**
+ * flip_bits - get number of bits you would need to flip to convert numbers
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits that needed to be flipped
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int flip_counter = 0;
+	unsigned long dummy;
+
+	dummy = (n | m) & (~(n & m));
+	while (dummy)
+	{
+		flip_counter += dummy & 1;
+		dummy >>= 1;
+	}
+
+	return (flip_counter);
+}
