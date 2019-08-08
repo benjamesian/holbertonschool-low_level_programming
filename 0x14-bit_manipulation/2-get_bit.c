a80a897cf2b2304fc0e055b2baaacaea72cc8c71
Ben Cook <808@holbertonschool.com>
2019-08-08 13:51:41 -0700

    fix error checking

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,5 +7,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int max_bits = sizeof(unsigned long int) * 8;
+
+	if (index > max_bits)
+		return (-1);
+	
 	return ((n >> index) & 1);
 }
