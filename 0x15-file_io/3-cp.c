876b199c4bb154890ce87fa9a56786ec1509339f
Ben Cook <808@holbertonschool.com>
2019-08-13 15:49:14 -0700

    fix betty issues

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,14 +4,29 @@
 #define BUFSIZE 1024
 
 /**
+ * close_file - close a file and return an error message on failure
+ * @fd: file descriptor of file to close
+ */
+void close_file(int fd)
+{
+
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
  * main - entry point
  * @argc: number of args provided to program
  * @argv: arguments provided to the program
+ *
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	int fdf, fdt, closed, bytes_read = BUFSIZE, bytes_written;
-	char *ft, *ff;
+	int fdf, fdt, bytes_read = BUFSIZE;
 	char buf[BUFSIZE];
 
 	if (argc != 3)
@@ -20,55 +35,33 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	ff = argv[1];
-	ft = argv[2];
-
-	fdf = open(ff, O_RDONLY);
+	fdf = open(argv[1], O_RDONLY);
 	if (fdf == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", ff);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	fdt = open(ft, O_WRONLY | O_TRUNC | O_CREAT, 0664);
-
+	fdt = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (fdt == -1)
 	{
-		closed = close(fdf);
-		if (closed == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (int) fdf);
-			exit(100);
-		}
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", ft);
+		close_file(fdf);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 
 	while (bytes_read == BUFSIZE)
 	{
 		bytes_read = read(fdf, buf, BUFSIZE);
-		printf("Writing... %s", buf);
-		bytes_written = write(fdt, buf, bytes_read);
-		if (bytes_written == -1)
+		if (write(fdt, buf, bytes_read) == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", ft);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
 
-	closed = close(fdf);
-	if (closed == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (int) fdf);
-		exit(100);
-	}
-
-	closed = close(fdt);
-	if (closed == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (int) fdt);
-		exit(100);
-	}
+	close_file(fdf);
+	close_file(fdt);
 
 	return (0);
 }
