f3bf387fc6358de05bcea4c357c959fafdfb8145
Ben Cook <808@holbertonschool.com>
2019-11-15 15:16:49 -0800

    remove helper function

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,10 +8,6 @@ def island_perimeter(grid):
     for i, row in enumerate(grid):
         for j, entry in enumerate(row):
             if entry == 1:
-                perimeter += count_neighbor_zeros(grid, i, j)
+                perimeter += 4 - sum(
+                    [grid[i+1][j], grid[i-1][j], grid[i][j+1], grid[i][j-1]])
     return perimeter
-
-
-def count_neighbor_zeros(grid, i, j):
-    """Returns number of zeros surrounding a grid index."""
-    return 4 - sum([grid[i+1][j], grid[i-1][j], grid[i][j+1], grid[i][j-1]])
