d5922f9d0608e880d4da2ad3335e13a2a41ddf1d
Ben Cook <808@holbertonschool.com>
2019-12-05 14:26:54 -0800

    attempt is_bst fix

--- 0x1D-binary_trees/110-binary_tree_is_bst.c
+++ 0x1D-binary_trees/110-binary_tree_is_bst.c
@@ -1,24 +1,36 @@
 #include "binary_trees.h"
-#include <limits.h>
 
 /**
  * _binary_tree_is_bst - recursively check if a binary tree is a bst
- * @tree: tree to inspect
- * @max_right: max value a right node can hold
- * @min_left: min value a left child can have
+ * @node: node to inspect
+ * @prev: a parent of the current node or the same as node
  *
  * Return: 1 if tree is a bst, else 0
  */
-int _binary_tree_is_bst(const binary_tree_t *tree, int max_right, int min_left)
+int _binary_tree_is_bst(const binary_tree_t *node, const binary_tree_t *prev)
 {
-	if (!tree)
+	if (!node)
 		return (1);
-	if (tree->left && (tree->left->n > tree->n || tree->left->n < min_left))
-		return (0);
-	if (tree->right && (tree->right->n < tree->n || tree->right->n > max_right))
-		return (0);
-	return (_binary_tree_is_bst(tree->left, tree->n, min_left)
-		&& _binary_tree_is_bst(tree->right, max_right, tree->n));
+
+	if (node->n == prev->n)
+	{
+		if ((node->left && node->left->n > prev->n)
+			|| (node->right && node->right->n < prev->n))
+			return (0);
+	}
+	else if (node->n < prev->n)
+	{
+		if ((node->left && node->left->n > prev->n)
+			|| (node->right && node->right->n > prev->n))
+			return (0);
+	}
+	else
+	{
+		if ((node->left && node->left->n < prev->n)
+			|| (node->right && node->right->n < prev->n))
+			return (0);
+	}
+	return (_binary_tree_is_bst(node->left, prev) && _binary_tree_is_bst(node->right, prev));
 }
 
 /**
@@ -30,6 +42,7 @@ int _binary_tree_is_bst(const binary_tree_t *tree, int max_right, int min_left)
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (0);
-	return (_binary_tree_is_bst(tree, INT_MAX, INT_MIN));
+		return (1);
+
+	return (_binary_tree_is_bst(tree, tree) && _binary_tree_is_bst(tree->left, tree->left) && _binary_tree_is_bst(tree->right, tree->right));
 }
