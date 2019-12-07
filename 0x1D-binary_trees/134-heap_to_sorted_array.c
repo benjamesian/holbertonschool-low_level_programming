44c21ee02a8355accfc17383225b64206e743bfc
Ben Cook <808@holbertonschool.com>
2019-12-07 00:11:30 -0800

    fix interpreatation of size param

--- 0x1D-binary_trees/134-heap_to_sorted_array.c
+++ 0x1D-binary_trees/134-heap_to_sorted_array.c
@@ -1,6 +1,22 @@
 #include "binary_trees.h"
 
 /**
+ * __binary_tree_size - get the size of a binary tree
+ * @tree: tree to find the size of
+ *
+ * Return: size of the tree.
+ */
+size_t __binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (1 +
+		__binary_tree_size(tree->left) +
+		__binary_tree_size(tree->right));
+}
+
+/**
  * heap_to_sorted_array - convert a heap to a sorted array
  * @heap: heap to convert
  * @size: size of the heap
@@ -9,14 +25,19 @@
  */
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-	int *array, value;
+	int *array;
+	size_t i;
 
-	array = malloc(sizeof(*array) * (*size));
+	if (!heap)
+		return (NULL);
+
+	i = __binary_tree_size(heap);
+	array = malloc(sizeof(*array) * i);
 	if (!array)
 		return (NULL);
-	*size = *size - 1;
-	while ((value = heap_extract(&heap)))
-		*array++ = value;
-
+	*size = i;
+	for (i = 0; i < *size; i++)
+		array[i] = heap_extract(&heap);
+	
 	return (array);
 }
