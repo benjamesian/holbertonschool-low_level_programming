d9235787097294891a8691778e800054de5ff362
Ben Cook <808@holbertonschool.com>
2020-01-14 13:01:27 -0800

    add a newline to output

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d] = [%zu]", i, array[i]);
+		printf("Value checked array[%d] = [%zu]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
