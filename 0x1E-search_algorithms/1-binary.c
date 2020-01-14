f613d5561790329777561973c616e73188236a00
Ben Cook <808@holbertonschool.com>
2020-01-14 13:07:12 -0800

    fix off-by-one error

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,17 +10,25 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, low = 0, high = size;
+	size_t i, j, low = 0, high = size;
 
 	while (low < high)
 	{
-		i = (high - low) / 2;
+		printf("Searching in array: ");
+		for (j = low; j < high; j++)
+		{
+			printf("%d", array[j]);
+			if (j < high - 1)
+				printf(", ");
+		}
+		printf("\n");
+		i = low + (high - low) / 2;
 		if (array[low + i] == value)
 			return (i);
 		else if (array[i] < value)
 			low = i + 1;
 		else
-			high = i - 1;
+			high = i;
 	}
 
 	return (-1);
