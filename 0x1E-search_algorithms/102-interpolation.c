1be1676b77d925071b2b566c78c3c6a24d2430e8
Ben Cook <808@holbertonschool.com>
2020-01-14 14:26:11 -0800

    attempt interpolation search algorithm

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - interpolation search algorithm
+ * @array: array to search
+ * @size: size of the array
+ * @value: value to search for
+ *
+ * Return: some index where value is found or -1
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t l = 0, h = size - 1;
+
+	while (l < h)
+	{
+		l += (((double)(h - l) / (array[h] - array[l])) * (value - array[l]));
+		if (l < size)
+			printf("Value checked array[%lu] = [%d]\n", l, array[l]);
+		else
+		{
+			printf("Value checked array[%lu] is out of range\n", l);
+			break;
+		}
+
+		if (array[l] == value)
+			return (l);
+	}
+
+	return (-1);
+}
