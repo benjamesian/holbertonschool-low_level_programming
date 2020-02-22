78a14d0f13bce6ef84d92459169118f1003fb16f
Ben Cook <808@holbertonschool.com>
2020-02-21 19:26:20 -0800

    fix allocation of array in hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,21 +9,19 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
-	hash_node_t **arr;
 
 	ht = malloc(sizeof(*ht));
 	if (!ht)
 		return (NULL);
 
-	arr = calloc(sizeof(*arr), size);
-	if (!arr)
+	ht->array = calloc(size, sizeof(*ht->array));
+	if (!ht->array)
 	{
 		free(ht);
 		return (NULL);
 	}
 
 	ht->size = size;
-	ht->array = arr;
 
 	return (ht);
 }
